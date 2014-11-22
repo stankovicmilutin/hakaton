ee5b59babf175cb1e9415b89a5edfbcebf83cd31
Milutin <lordmilutin@gmail.com>
2014-11-22 22:25:41 +0100

    start

---
+++ app/database/seeds/BazaSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+class BazaSeeder extends Seeder{
+    
+    public function run(){
+        Predmet::create( array( 'naziv' => 'Softversko Inženjerstvo', 'tip' => 'Obavezni', 'espb' => '6' ));
+        Predmet::create( array( 'naziv' => 'Softversko Inženjerstvo2', 'tip' => 'Obavezni', 'espb' => '6' ));
+        Predmet::create( array( 'naziv' => 'Softversko Inženjerstvo3', 'tip' => 'Obavezni', 'espb' => '6' ));
+        Predmet::create( array( 'naziv' => 'Softversko Inženjerstvo4', 'tip' => 'Obavezni', 'espb' => '6' ));
+        Predmet::create( array( 'naziv' => 'Softversko Inženjerstvo5', 'tip' => 'Obavezni', 'espb' => '6' ));
+        Predmet::create( array( 'naziv' => 'Softversko Inženjerstvo6', 'tip' => 'Obavezni', 'espb' => '6' ));
+        Predmet::create( array( 'naziv' => 'Softversko Inženjerstvo7', 'tip' => 'Obavezni', 'espb' => '6' ));
+        Predmet::create( array( 'naziv' => 'Softversko Inženjerstvo8', 'tip' => 'Obavezni', 'espb' => '6' ));
+        Predmet::create( array( 'naziv' => 'Softversko Inženjerstvo9', 'tip' => 'Obavezni', 'espb' => '6' ));
+        Predmet::create( array( 'naziv' => 'Softversko Inženjerstvo0', 'tip' => 'Obavezni', 'espb' => '6' ));
+        Predmet::create( array( 'naziv' => 'Softversko Inženjerstvo-', 'tip' => 'Obavezni', 'espb' => '6' ));
+        
+
+    }
+    
+    
+}
