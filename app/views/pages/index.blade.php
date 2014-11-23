236e8893a2e6517e7407f44e12f9dee9d698e5ea
Milutin <lordmilutin@gmail.com>
2014-11-23 07:20:02 +0100

    asd

--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -2,26 +2,44 @@
 
 
 @section('content')
-    <div class="widget one">
-        <h3>Ukupno dugovanje</h3><h3><span>28</span>dana</h3>
-    </div>
-    <div class="widget sec">
-        <h3>Do ispitnog roka preostalo je još</h3><h3><span>28</span>dana</h3>
-    </div>
-    <div class="widget thrd">
-        <h3>Ukupno dugovanje</h3><h3><span>14.000</span>RSD</h3>
-    </div>
-    <div class="clearfix"></div>
-    <div class="thead"><h2>Podsetnik obaveza </h2><h6 class="txt-right">Datum</h6><h6 class="txt-right preostalo">Preostalo</h6><h6 class="txt-right">Detalji</h6></div>
-    <div class="btn-1"><span></span><h6>Predstojeći ispit</h6><h6>Računarsko programiranje</h6><h6 class="txt-right">28, Novembar 2014</h6><h6 class="txt-right preostalo">još 5 dana</h6><div><span></span></div></div>
-    <div class="btn-1"><span></span><h6>Predstojeći ispit</h6><h6>Matematika</h6><h6 class="txt-right">31, Novembar 2014</h6><h6 class="txt-right preostalo">još 8 dana</h6><div><span></span></div></div>
-    <div class="btn-1"><span></span><h6>Ispitni rok</h6><h6>Početak prijave za januarski ispitni rok</h6><h6 class="txt-right">15, Decembar 2014</h6><h6 class="txt-right preostalo">još 18 dana</h6><div><span></span></div></div>
-    <div class="btn-1"><span></span><h6>Školarina</h6><h6>Decembarska rata</h6><h6 class="txt-right">20, Decembar 2014</h6><h6 class="txt-righ preostalo">još 23 dana</h6><div><span></span></div></div>
-    <div class="line"></div>
-    <div class="clearfix"></div>
-    <section>
-        <div></div>
-        <h4 class="lrg">dreamspark program</h4>
-        <p></p>
-    </section>
+<div class="widget one">
+                <h3>Ukupno dugovanje</h3><h3><span>28</span>dana</h3>
+            </div>
+            <div class="widget sec">
+                <h3>Do ispitnog roka preostalo je još</h3><h3><span>28</span>dana</h3>
+            </div>
+            <div class="widget thrd">
+                <h3>Ukupno dugovanje</h3><h3><span>14.000</span>RSD</h3>
+            </div>
+            <div class="clearfix"></div>
+            <div class="thead"><h2>Podsetnik obaveza </h2><h6 class="txt-right">Datum</h6><h6 class="txt-right preostalo">Preostalo</h6><h6 class="txt-right">Detalji</h6></div>
+            <div class="btn-1 active"><span></span><h6>Predstojeći ispit</h6><h6>Računarsko programiranje</h6><h6 class="txt-right">28, Novembar 2014</h6><h6 class="txt-right preostalo">još 5 dana</h6><div><span></span></div></div>
+            <div class="btn-1"><span></span><h6>Predstojeći ispit</h6><h6>Matematika</h6><h6 class="txt-right">31, Novembar 2014</h6><h6 class="txt-right preostalo">još 8 dana</h6><div><span></span></div></div>
+            <div class="btn-1"><span></span><h6>Ispitni rok</h6><h6>Početak prijave za januarski ispitni rok</h6><h6 class="txt-right">15, Decembar 2014</h6><h6 class="txt-right preostalo">još 18 dana</h6><div><span></span></div></div>
+            <div class="btn-1"><span></span><h6>Školarina</h6><h6>Decembarska rata</h6><h6 class="txt-right">20, Decembar 2014</h6><h6 class="txt-righ preostalo">još 23 dana</h6><div><span></span></div></div>
+            <div class="line"></div>
+            <div class="clearfix"></div>
+            <section class="news">
+                <div class="date">
+                    <div class="day">18</div>
+                    <div class="mnth">NOV</div>
+                </div>
+                <section class="artcl">
+                   <h4 class="lrg">dreamspark program</h4>
+                    <p>Nakon višenedeljne komunikacije sa kompanijom koja realizuje Microsoftov DreamSPark program otklonjeni su problemi koji su Vas sprečavali da preuzmete besplatan softver. Svi nalozi su migrirani u novu prodavnicu, tebi sada trebalo da ste već dobili obaveštenje od kivuto.com putem email-a. </p>
+                    <p>Izvinjavamo se zbog problema koje ste imali, ali definitivno nije bio problem kod nas. </p>
+                </section>
+            </section>
+            <section class="news">
+                <div class="date">
+                    <div class="day">13</div>
+                    <div class="mnth">NOV</div>
+                </div>
+                <section class="artcl">
+                    <h4 class="lrg">dreamspark program</h4>
+                    <p>Nakon višenedeljne komunikacije sa kompanijom koja realizuje Microsoftov DreamSPark program otklonjeni su problemi koji su Vas sprečavali da preuzmete besplatan softver. Svi nalozi su migrirani u novu prodavnicu, tebi sada trebalo da ste već dobili obaveštenje od kivuto.com putem email-a. </p>
+                    <p>Izvinjavamo se zbog problema koje ste imali, ali definitivno nije bio problem kod nas. </p>
+                </section>
+                <div class="clearfix"></div>
+            </section>
 @stop
--- app/views/pages/skolarina.blade.php
+++ app/views/pages/skolarina.blade.php
@@ -2,15 +2,20 @@
 
 
 @section('content')
-    @foreach($rate as $rata)
-    
-    <div class="btn-1">
-        <span></span>
-        <h6>{{ Funkcije::phpDatum($rata->rok) }}</h6>
-        <h6>Rata za školarinu</h6>
-        <h6 class="txt-right">{{ $rata->iznos }} RSD</h6>
+<div class="thead s">
+        <h2>Školarina </h2>
+        <h6 class="txt-right">Rok za plaćanje</h6>
+        <h6 class="txt-right preostalo">Iznos</h6>
+        <h6 class="txt-right"></h6>
+ </div>
+@foreach($rate as $rata)
+
+    <div class="btn-1 active"><span></span>
+        <h6 class="txt-right">{{ Funkcije::phpDatum($rata->rok) }}</h6>
+        <h6 class="txt-right preostalo">{{ $rata->iznos }} RSD</h6>
         <h6 class="txt-right preostalo">PLATI</h6>
         <div><span></span></div>
     </div>
-    @endforeach
+
+@endforeach
 @stop
